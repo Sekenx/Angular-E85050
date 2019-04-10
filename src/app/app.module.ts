12f9b499ba8b3d2f99504c1da8181d2e95de6783
Sekenx <seken@gmx.com>
2019-04-10 16:08:34 +0100

    2 files modified

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
+import { RouterModule} from '@angular/router';
 
 import { AppComponent } from './app.component';
 import { HelloComponent } from './hello.component';
@@ -8,19 +9,19 @@ import { TeamComponent } from './team/team.component';
 import { HomeComponent } from './home/home.component';
 import { NavbarComponent } from './navbar/navbar.component';
 
-const appRoutes:Routes = [
-  {
-    path: '',
-    component: AppComponent
-  },
-  {
-    path: 'home',
-    component: AppComponent
-  }
-]
+RouterModule.forRoot([
+   {
+      path: '',
+      component: HomeComponent
+   },
+   {
+     path: 'app-home',
+     component: HomeComponent
+   }
+])
 
 @NgModule({
-  imports:      [ BrowserModule, FormsModule ],
+  imports:      [ BrowserModule, FormsModule, RouterModule ],
   declarations: [ AppComponent, HelloComponent, TeamComponent, HomeComponent, NavbarComponent ],
   bootstrap:    [ AppComponent ]
 })
